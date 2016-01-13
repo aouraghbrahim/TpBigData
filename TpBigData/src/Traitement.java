94d7145cc157f2cbf726b7029ae2855ab9800058
b.aouragh.13 <b.aouragh.13@INFO-182-16.intranet.int>
2016-01-13 15:34:46 +0100

    exo

--- TpBigData/src/Traitement.java
+++ TpBigData/src/Traitement.java
@@ -1,7 +1,23 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 
 public class Traitement {
 
 	public static void main(String[] args) {
-	//Francois	
+	
+		FileInputStream fstream = null;
+		try {
+			fstream = new FileInputStream("movies.dat");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			ObjectInputStream ois = new ObjectInputStream(fstream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}  
 	}
 }
