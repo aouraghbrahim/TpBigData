6557e1e8b08ea1fa3eacf9281f7afda73dd4be22
b.aouragh.13 <b.aouragh.13@INFO-182-16.intranet.int>
2016-01-13 16:19:45 +0100

    read file

--- TpBigData/src/Traitement.java
+++ TpBigData/src/Traitement.java
@@ -1,25 +1,26 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
 
 public class Traitement {
 
-	public static void main(String[] args) {
-	
-		File f = new File("C:\Users\b.aouragh.13\Desktop\MovieLens\movies.dat");
+	public static void main(String[] args) throws IOException {
+		ObjectInputStream ois = null;
+		File f = new File("C:\\Users\\b.aouragh.13\\Desktop\\MovieLens\\ml-10M100K\\movies.dat");
 		FileInputStream fstream = null;
-		try {
-			fstream = new FileInputStream("f");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		
+		FileReader file = new FileReader(new File("C:\\Users\\b.aouragh.13\\Desktop\\MovieLens\\ml-10M100K\\movies.dat"));
+		BufferedReader br = new BufferedReader(file);
+		String temp = br.readLine();
+		while (temp != null) {
+		   temp = br.readLine();
+		   System.out.println(temp);
 		}
-		try {
-			ObjectInputStream ois = new ObjectInputStream(fstream);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}  
+		
 	}
 }
