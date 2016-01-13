00d0bad3670c75988a34e280af023e35ceeed396
b.aouragh.13 <b.aouragh.13@INFO-182-16.intranet.int>
2016-01-13 16:20:45 +0100

    readfile

--- TpBigData/src/Traitement.java
+++ TpBigData/src/Traitement.java
@@ -10,10 +10,8 @@ import java.io.ObjectInputStream;
 public class Traitement {
 
 	public static void main(String[] args) throws IOException {
-		ObjectInputStream ois = null;
-		File f = new File("C:\\Users\\b.aouragh.13\\Desktop\\MovieLens\\ml-10M100K\\movies.dat");
-		FileInputStream fstream = null;
 		
+
 		FileReader file = new FileReader(new File("C:\\Users\\b.aouragh.13\\Desktop\\MovieLens\\ml-10M100K\\movies.dat"));
 		BufferedReader br = new BufferedReader(file);
 		String temp = br.readLine();
@@ -21,6 +19,7 @@ public class Traitement {
 		   temp = br.readLine();
 		   System.out.println(temp);
 		}
+		
 		
 	}
 }
