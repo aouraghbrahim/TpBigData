c9e6f6ce66bce7e0a5fc1ced3aa99e2a148c54dd
b.aouragh.13 <b.aouragh.13@INFO-182-16.intranet.int>
2016-01-13 16:58:13 +0100

    result

--- TpBigData/src/Traitement.java
+++ TpBigData/src/Traitement.java
@@ -1,25 +1,32 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
 
 
 public class Traitement {
 
 	public static void main(String[] args) throws IOException {
 		
+		 ArrayList<String> tem = new ArrayList<String>();
 
-		FileReader file = new FileReader(new File("C:\\Users\\b.aouragh.13\\Desktop\\MovieLens\\ml-10M100K\\movies.dat"));
+		FileReader file = new FileReader(new File("C:\\Users\\b.aouragh.13\\Desktop\\MovieLens\\ml-100k\\u.data"));
 		BufferedReader br = new BufferedReader(file);
-		String temp = br.readLine();
-		while (temp != null) {
-		   temp = br.readLine();
-		   System.out.println(temp);
+		String t;
+		while ((t = br.readLine()) != null)
+		{
+		    tem.add(t);
+		   
 		}
-		
+		String result = null;
+		for(int i=0;i<tem.size();i++){
+			 result=tem.get(i);	
+			 String values[]=result.split("	");
+			 
+			 System.out.println(values[i]);
+		}
 		
 	}
 }
